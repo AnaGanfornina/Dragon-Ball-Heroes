1a9a352238649e289357bae414eea735f6fd4508
Ana Ganfornina <anies.llamani@gmail.com>
2024-09-16 11:28:11 +0200

    feat: MVC

---
+++ Dragon_Ball_Heroes/Models/DBCharacter.swift
@@ -0,0 +1,13 @@
+//
+//  DBCharacter.swift
+//  Dragon_Ball_Heroes
+//
+//  Created by Ana on 16/9/24.
+//
+
+import Foundation
+//TODO: hay que ver si esto es verdaderamente la descripcion de los personajes o es lo de las transformaciones
+struct DBCharacter{
+    let fullNmae: String
+    let description: String
+}
---
+++ Dragon_Ball_Heroes/Models/Heroes.swift
@@ -0,0 +1,13 @@
+//
+//  Heroes.swift
+//  Dragon_Ball_Heroes
+//
+//  Created by Ana on 16/9/24.
+//
+
+import Foundation
+enum Heroes: String, CaseIterable{
+    case goku = "Goku"
+    case vegeta = "Vegeta"
+    case Bulma = "Bulma"
+}
